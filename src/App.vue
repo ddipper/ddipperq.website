4dd7e83d6cc21083474f1102036a25d2621c6b29
Dmitry <dev.dbuts@gmail.com>
2024-08-23 00:43:53 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="header-page-links">
+      <router-link :to="{ name: 'Home' }">home</router-link>
+      <router-link :to="{ name: 'About' }">about</router-link>
+      <router-link :to="{ name: 'Contact' }">contact</router-link>
+    </div>
+    <div class="header-other-links">
+      <a>ip</a>
+      <h3><span class="purple-color">/</span></h3>
+      <a>???</a>
+    </div>
+    <div class="header-time">
+      <h3>time: 11:10:09 PM</h3>
+    </div>
+  </header>
+  <router-view>
+  </router-view>
+</template>
+
+<style scoped lang="scss">
+  @import './assets/colors';
+
+  header{
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 20px 8px 20px;
+    margin: 20px 20px 0 20px;
+    border-radius: 10px;
+    background-color: $header-color;
+    color: $text-color;
+    font-size: 1.25rem;
+    font-weight: 400;
+
+    a{
+      color: $text-color;
+      text-decoration: underline;
+    }
+
+    h3{
+      font-size: 1.25rem;
+      font-weight: 400;
+    }
+
+    .header-page-links{
+      display: flex;
+      gap: 20px;
+    }
+
+    .header-other-links{
+      display: flex;
+      gap: 5px;
+      h3{
+        margin-bottom: 3px;
+      }
+    }
+
+  }
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import Contact from './components/Contact.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {path: '/', name: 'Home',component: Home},
+        {path: '/about', name: 'About', component: About},
+        {path: '/contact', name: 'Contact', component: Contact}
+    ]
+    }
+)
+
+export default router
