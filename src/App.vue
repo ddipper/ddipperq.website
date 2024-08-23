701b3805192eb75453d486725bf52844ad17837a
Dmitry <dev.dbuts@gmail.com>
2024-08-23 03:05:53 +0300

    add basics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
-<script setup>
+<script>
+import NavigationButtons from './components/models/NavigationButtons.vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 
+export default {
+  setup() {
+    const header = ref(null);
+    const bodyContentHeight = ref('100vh');
+
+    const adjustConsoleHeight = () => {
+      if (header.value) {
+        const headerHeight = header.value.offsetHeight;
+        const headerMarginTop = parseFloat(getComputedStyle(header.value).marginTop);
+        const headerMarginBottom = parseFloat(getComputedStyle(header.value).marginBottom);
+        const totalHeaderHeight = headerHeight + headerMarginTop + headerMarginBottom;
+        bodyContentHeight.value = `calc(100vh - ${totalHeaderHeight}px)`;
+        console.log(bodyContentHeight.value);
+      }
+    };
+
+    onMounted(() => {
+      adjustConsoleHeight();
+      window.addEventListener('resize', adjustConsoleHeight);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', adjustConsoleHeight);
+    });
+
+    return {
+      header,
+      bodyContentHeight,
+    };
+  },
+  components: {
+    NavigationButtons
+  }
+};
 </script>
 
 <template>
-  <header>
+  <header ref="header">
     <div class="header-page-links">
       <router-link :to="{ name: 'Home' }">home</router-link>
       <router-link :to="{ name: 'About' }">about</router-link>
@@ -15,11 +51,24 @@
       <a>???</a>
     </div>
     <div class="header-time">
-      <h3>time: 11:10:09 PM</h3>
+      <h3><span class="orange-color">time:</span> 11:10:09 PM</h3>
     </div>
   </header>
-  <router-view>
-  </router-view>
+  <div class="body-content" :style="{ height: bodyContentHeight }">
+    <div class="console">
+      <div class="console-header">
+        <div class="console-header-circles-group">
+          <div class="console-header-circle"></div>
+          <div class="console-header-circle"></div>
+          <div class="console-header-circle"></div>
+        </div>
+        <h3 class="console-header-title">PAGE_TITLE</h3>
+      </div>
+      <router-view>
+      </router-view>
+      <NavigationButtons />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -56,9 +105,61 @@
       gap: 5px;
       h3{
         margin-bottom: 3px;
+        font-weight: bold;
       }
     }
+  }
 
+  .body-content {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .console {
+      background-color: $console-color;
+      width: 1300px;
+      overflow: hidden;
+      border-radius: 10px;
+      height: 800px;
+
+      .console-header{
+        width: calc(100% - 32px);
+        padding: 16px;
+        background-color: $console-header-color;
+        display: flex;
+
+        .console-header-circles-group{
+          display: flex;
+          align-items: center;
+          gap: 12px;
+
+          .console-header-circle{
+            width: 12px;
+            height: 12px;
+            border-radius: 50%;
+          }
+          .console-header-circle:nth-child(1){
+            background-color: $orange-color;
+          }
+          .console-header-circle:nth-child(2){
+            background-color: $purple-color;
+          }
+          .console-header-circle:nth-child(3){
+            background-color: $green-color;
+          }
+        }
+
+        .console-header-title{
+          color: $text-color;
+          font-size: 1.5rem;
+          font-weight: 400;
+          width: 100%;
+          text-align: center;
+          margin-right: 60px;
+        }
+      }
+    }
   }
 
 </style>
---
+++ src/components/models/NavigationButtons.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="nav-buttons">
+    <button @click="prevPage">prev</button>
+    <h3 class="purple-color">/</h3>
+    <button @click="nextPage">next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pages: ['Home', 'About', 'Contact'],
+      currentIndex: 0
+    };
+  },
+  methods: {
+    nextPage() {
+      this.currentIndex = (this.currentIndex + 1) % this.pages.length;
+      this.$router.push({ name: this.pages[this.currentIndex] });
+    },
+    prevPage() {
+      this.currentIndex = (this.currentIndex - 1 + this.pages.length) % this.pages.length;
+      this.$router.push({ name: this.pages[this.currentIndex] });
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.nav-buttons{
+  display: flex;
+  gap: 6px;
+  align-items: center;
+
+  button{
+    background-color: transparent;
+    border: none;
+    font-family: 'JetBrains Mono', monospace;
+    font-size: 1.4rem;
+    font-weight: 400;
+    text-decoration: underline;
+
+    &:hover{
+      cursor: pointer;
+      opacity: 0.3;
+      transition: .25s cubic-bezier(0,0,.2,1);
+    }
+    &:not(:hover){
+      transition: .25s cubic-bezier(0,0,.2,1);
+    }
+  }
+  h3{
+    font-size: 1.4rem;
+    font-weight: bold;
+  }
+}
+</style>
